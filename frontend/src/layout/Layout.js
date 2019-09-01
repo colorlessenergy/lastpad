aa242b7f7abe2a600953c994e4eb472048226f24
Daniel Munoz <daniel.munozdev@gmail.com>
2019-09-01 08:50:18 -0700

    add HashRouter as per issue #9

--- frontend/src/layout/Layout.js
+++ frontend/src/layout/Layout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter, 
-          Route} from 'react-router-dom';
+import { HashRouter, Route} from 'react-router-dom';
 
 
 import Register from '../container/Register/Register';
@@ -9,10 +8,10 @@ import Login from '../container/Login/Login';
 
 function Layout () {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={Login} />
-    </BrowserRouter>
+    </HashRouter>
   )
 };
 
