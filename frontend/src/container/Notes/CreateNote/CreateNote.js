ad6c7f9aa741a4c037f6773b2b1dc9f5359d0fec
colorlessenergy <brianmunozdev@gmail.com>
2019-10-09 12:45:38 -0700

    remove some features from rich text editor and add margin to content page

--- frontend/src/container/Notes/CreateNote/CreateNote.js
+++ frontend/src/container/Notes/CreateNote/CreateNote.js
@@ -21,20 +21,16 @@ class CreateNote extends Component {
 
   modules = {
     toolbar: [
-      [{ header: '1' }, { header: '2' }, { font: [] }],
-      [{ size: [] }],
+      [{ header: '1' }, { header: '2' }],
       ['bold', 'italic', 'underline', 'strike', 'blockquote'],
       [{ list: 'ordered' }, { list: 'bullet' }],
       ['link'],
-      ['clean'],
       ['code-block']
     ]
   };
 
   formats = [
     'header',
-    'font',
-    'size',
     'bold',
     'italic',
     'underline',
